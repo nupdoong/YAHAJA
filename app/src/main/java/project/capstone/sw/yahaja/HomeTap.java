0452c2e1e3bc75bf2ac312f29167e8729855c265
Daehyun <eogussla12@gmail.com>
2018-12-05 20:50:48 +0900

    map

--- app/src/main/java/project/capstone/sw/yahaja/HomeTap.java
+++ app/src/main/java/project/capstone/sw/yahaja/HomeTap.java
@@ -1,10 +1,13 @@
 package project.capstone.sw.yahaja;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+import android.widget.Button;
 
 public class HomeTap extends Fragment {
     public HomeTap(){}
@@ -13,8 +16,19 @@ public class HomeTap extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
+        Button btn_server = (Button) view.findViewById(R.id.btn_server);
+
+        btn_server.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(HomeTap.super.getActivity(), MapActivity.class);
+                startActivity(intent);
+            }
+        });
+
         return view;
     }
+
 
 
 }
