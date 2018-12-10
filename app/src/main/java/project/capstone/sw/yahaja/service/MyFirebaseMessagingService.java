d19a8dd20383bd362b7aa462a86a5b6550e14fb2
Daehyun <eogussla12@gmail.com>
2018-12-11 02:48:25 +0900

    match

--- app/src/main/java/project/capstone/sw/yahaja/service/MyFirebaseMessagingService.java
+++ app/src/main/java/project/capstone/sw/yahaja/service/MyFirebaseMessagingService.java
@@ -1,5 +1,9 @@
 package project.capstone.sw.yahaja.service;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
@@ -18,13 +22,29 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
             String body = remoteMessage.getNotification().getBody();
             Log.d(TAG, "Notification Body: " + body);
 
-            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(getApplicationContext())
+
+            if (Build.VERSION.SDK_INT < 26) {
+                return;
+            }else {
+                NotificationManager notificationManager =
+                        (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+
+                NotificationChannel channel = new NotificationChannel("default",
+                        "Channel name",
+                        NotificationManager.IMPORTANCE_DEFAULT);
+                channel.setDescription("Channel description");
+                notificationManager.createNotificationChannel(channel);
+
+            }
+
+            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(getApplicationContext(), "default")
                     .setSmallIcon(R.mipmap.ic_launcher) // 알림 영역에 노출 될 아이콘.
                     .setContentTitle(getString(R.string.app_name)) // 알림 영역에 노출 될 타이틀
-                    .setContentText(body); // Firebase Console 에서 사용자가 전달한 메시지내용)
+                    .setContentText(body)
+                    .setOngoing(true); // Firebase Console 에서 사용자가 전달한 메시지내용)
 
             NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(getApplicationContext());
-            notificationManagerCompat.notify(0x1001, notificationBuilder.build());
+            notificationManagerCompat.notify(0, notificationBuilder.build());
         }
     }
 }
